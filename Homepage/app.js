f1958e5f47e0a88c8d8cb5f6b70089cd74a737e8
Nguyenhoang291299 <hoang11761311@gmail.com>
2022-09-14 00:02:50 +0700

    main

--- Homepage/app.js
+++ Homepage/app.js
@@ -3,7 +3,9 @@ const closeMenu = document.querySelector('.bi-x-lg');
 const bgSidebar = document.querySelector('.bg-sidebar');
 const submenuRes = document.querySelectorAll('.sub-menu-items b');
 const subChilds = document.querySelectorAll('.sub-menu-items ul');
-const hire = document.querySelector('.hire');
+const btnSearch = document.querySelector('.bi-search');
+const closeSearch = document.querySelector('.sf-close');
+const headerSearch = document.querySelector('.header_search');
 // responsive menu side bar
 openMenu.onclick = function () {
     bgSidebar.style.display = "block";
@@ -11,13 +13,26 @@ openMenu.onclick = function () {
 closeMenu.onclick = function () {
     bgSidebar.style.display = "none";
 }
+// OC search
+closeSearch.onclick = function () {
+    headerSearch.style.display = "none";
+}
+btnSearch.onclick = function () {
+    headerSearch.style.display = "block";
+}
 // handle slide slider-syncing
 
 $('.slide-content').slick({
-        slidesToShow: 3,
-        slidesToScroll: 1,
-        // asNavFor: '.slider-for',
-        dots: false,
-        centerMode: true,
-        focusOnSelect: true
+    slidesToShow: 4,
+    arrows: true,
+    responsive: [{
+        breakpoint: 1024,
+        setting: {
+            slidesToShow: 3,
+            breakpoint: 480,
+            settings: {
+                slidesToShow: 2
+            }
+        }
+    }]
   });
