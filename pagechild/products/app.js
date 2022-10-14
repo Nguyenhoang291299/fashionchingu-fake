e7e42b15f217e454870f0644ba65946b4eb530f2
Nguyenhoang291299 <hoang11761311@gmail.com>
2022-10-14 12:01:33 +0700

    main

--- pagechild/products/app.js
+++ pagechild/products/app.js
@@ -1,12 +1,10 @@
-
 const products = document.querySelector('.products');
-const prevPage = document.querySelector('.bi-caret-left-fill');
-const nextPage = document.querySelector('.bi-caret-right-fill');
-const clothing = document.querySelector("#clothing");
+const product = products.children;
 
-// const img = document.querySelector('.img');
-// const name = document.querySelector('.name');
-// const price = document.querySelector('.price');
+// const prevPage = document.querySelector('.bi-caret-left-fill');
+// const nextPage = document.querySelector('.bi-caret-right-fill');
+const clothing = document.querySelector("#clothing");
+const prices = document.querySelector("#price");
 
 const courseApi = `http://localhost:3000/products`;
 function start() {
@@ -19,13 +17,12 @@ function getCourse(callback) {
         .then(function(response) {
             return response.json();
         })
-        .then(callback)
+        .then(callback) 
 }
-
 function renderCourse(courses) {
-    
-    const htmls = courses.map(course => {   
-        return `
+
+    const htmls = courses.map(course => { 
+        return html =  `
         <div class="product">
             <a href="#">
                 <div class="condition">${course.condition}</div>
@@ -42,15 +39,64 @@ function renderCourse(courses) {
         `;
     })
     products.innerHTML = htmls.join('');
-    // filter products by type
-    clothing.addEventListener('change', ()=> {
-        // clothingValue.value
-        const clothingValue = clothing.value
-        courses.filter(course => { 
-            if(course.type === clothingValue)
-            {
-                product.classList.add("hire")
-            };
-        });
-    });
+        // filter products by type
+         clothing.addEventListener('change', ()=> {
+            const clothingValue = clothing.value;
+            const selectedClothingArrs = [];
+            const filterProducts = document.querySelector('.filterProducts');
+            courses.filter(course => { 
+                if(course.type === clothingValue)
+                {
+                    selectedClothingArrs.push(course)
+                    if(selectedClothingArrs.length > 0) {
+                        products.style.display = 'none'
+                        const htmlss = selectedClothingArrs.map(course => { 
+                            return `
+                            <div class="product">
+                                <a href="#">
+                                    <div class="condition">${course.condition}</div>
+                                    <div  class="img-item">
+                                        <img src="${course.image.imgSub0}" alt="">
+                                    </div>
+                                    <div class="mt_10">
+                                        <div class="name">${course.name}</div>
+                                        <div class="color">${course.color}</div>
+                                        <div class="price">${course.price}$</div>
+                                    </div>
+                                </a>
+                            </div>
+                            `;
+                        })
+                        filterProducts.innerHTML = htmlss.join('');
+                    } 
+                };
+                if (clothingValue == "") {
+                    products.style.display = "flex"
+                }
+            });
+        });  
+    //sort prices in order
+    function sortPrices() {
+        prices.addEventListener('change', ()=> {
+            const pricesValue = prices.value;
+                if (pricesValue == 'lowtohigh') {
+                    courses.sort((a, b) =>  
+                        a.price - b.price
+                    ) 
+                    console.log(courses);
+                    return html
+                }
+                if (pricesValue == 'hightolow') {
+                    courses.sort((a, b) =>  
+                        b.price - a.price
+                    ) 
+                    console.log(courses);
+                }
+                if (pricesValue == "") {
+                    console.log(courses);
+                }
+            }
+        )
+    }
+    sortPrices();
 }
