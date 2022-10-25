54af07d00962c8f6b9b375944a353e56b389d659
Nguyenhoang291299 <hoang11761311@gmail.com>
2022-10-25 21:16:51 +0700

    main

--- pagechild/products/app.js
+++ pagechild/products/app.js
@@ -1,16 +1,19 @@
+
 const products = document.querySelector('.products');
 const product = products.children;
-
 const inputForm = document.querySelector('#input-form');
+const header = document.querySelector('#header');
+console.log(header);
 
 // const prevPage = document.querySelector('.bi-caret-left-fill');
 // const nextPage = document.querySelector('.bi-caret-right-fill');
 // const clothing = document.querySelector("#clothing");
-// const prices = document.querySelector("#price");
+const prices = document.querySelector("#price");
 
 const courseApi = `http://localhost:3000/products`;
 function start() {
     getCourse(renderCourse);
+
 }
 start()
 
@@ -22,11 +25,11 @@ function getCourse(callback) {
         .then(callback) 
 }
 function renderCourse(courses) {
-
+    showId(courses)
     const htmls = courses.map((course,index) => { 
         return `
         <div class="product" data-id="${index}">
-            <a href="#"  onclick="showId(${index})"">
+            <a href="#">
                 <div class="condition">${course.condition}</div>
                 <div  class="img-item">
                     <img src="${course.image.imgSub0}" alt="">
@@ -41,137 +44,144 @@ function renderCourse(courses) {
         `;
     })
     products.innerHTML = htmls.join('');
-        // filter products by type
-        //  clothing.addEventListener('change', ()=> {
-        //     const clothingValue = clothing.value;
-        //     const selectedClothingArrs = [];
-        //     const filterProducts = document.querySelector('.filterProducts');
-        //     courses.filter(course => { 
-        //         if(course.type === clothingValue)
-        //         {
-        //             selectedClothingArrs.push(course)
-        //             if(selectedClothingArrs.length > 0) {
-        //                 products.style.display = 'none'
-        //                 const htmlss = selectedClothingArrs.map(course => { 
-        //                     return `
-        //                     <div class="product">
-        //                         <a href="#">
-        //                             <div class="condition">${course.condition}</div>
-        //                             <div  class="img-item">
-        //                                 <img src="${course.image.imgSub0}" alt="">
-        //                             </div>
-        //                             <div class="mt_10">
-        //                                 <div class="name">${course.name}</div>
-        //                                 <div class="color">${course.color}</div>
-        //                                 <div class="price">${course.price}$</div>
-        //                             </div>
-        //                         </a>
-        //                     </div>
-        //                     `;
-        //                 })
-        //                 filterProducts.innerHTML = htmlss.join('');
-        //             } 
-        //         };
-        //         if (clothingValue == "") {
-        //             products.style.display = "flex"
-        //         }
-        //     });
-        // });  
+        //filter products by type
+         clothing.addEventListener('change', ()=> {
+            const clothingValue = clothing.value;
+            const selectedClothingArrs = [];
+            const filterProducts = document.querySelector('.filterProducts');
+            courses.filter(course => { 
+                if(course.type === clothingValue)
+                {
+                    selectedClothingArrs.push(course)
+                    if(selectedClothingArrs.length > 0) {
+                        products.style.display = 'none'
+                        const htmlss = selectedClothingArrs.map(course => { 
+                            return `
+                            <div class="product">
+                                <a href="#">
+                                    <div class="condition">${course.condition}</div>
+                                    <div  class="img-item">
+                                        <img src="${course.image.imgSub0}" alt="">
+                                    </div>
+                                    <div class="mt_10">
+                                        <div class="name">${course.name}</div>
+                                        <div class="color">${course.color}</div>
+                                        <div class="price">${course.price}$</div>
+                                    </div>
+                                </a>
+                            </div>
+                            `;
+                        })
+                        filterProducts.innerHTML = htmlss.join('');
+                    } 
+                };
+                if (clothingValue == "") {
+                    products.style.display = "flex"
+                }
+            });
+        });  
     //sort prices in order
-    // function sortPrices() {
-    //     prices.addEventListener('change', ()=> {
-    //         const pricesValue = prices.value;
-    //             if (pricesValue == 'lowtohigh') {
-    //                 courses.sort((a, b) =>  
-    //                     a.price - b.price
-    //                 ) 
-    //                 const htmls = courses.map(course => { 
-    //                     return `
-    //                     <div class="product">
-    //                         <a href="#">
-    //                             <div class="condition">${course.condition}</div>
-    //                             <div  class="img-item">
-    //                                 <img src="${course.image.imgSub0}" alt="">
-    //                             </div>
-    //                             <div class="mt_10">
-    //                                 <div class="name">${course.name}</div>
-    //                                 <div class="color">${course.color}</div>
-    //                                 <div class="price">${course.price}$</div>
-    //                             </div>
-    //                         </a>
-    //                     </div>
-    //                     `;
-    //                 })
-    //                 products.innerHTML = htmls.join('');
-    //             }
-    //             if (pricesValue == 'hightolow') {
-    //                 courses.sort((a, b) =>  
-    //                     b.price - a.price
-    //                 ) 
-    //                 const htmls = courses.map(course => { 
-    //     return `
-    //     <div class="product">
-    //         <a href="#">
-    //             <div class="condition">${course.condition}</div>
-    //             <div  class="img-item">
-    //                 <img src="${course.image.imgSub0}" alt="">
-    //             </div>
-    //             <div class="mt_10">
-    //                 <div class="name">${course.name}</div>
-    //                 <div class="color">${course.color}</div>
-    //                 <div class="price">${course.price}$</div>
-    //             </div>
-    //         </a>
-    //     </div>
-    //     `;
-    // })
-    // products.innerHTML = htmls.join('');
-    //             }
-    //             if (pricesValue == "") {
-    //                 const htmls = courses.map(course => { 
-    //     return `
-    //     <div class="product">
-    //         <a href="#">
-    //             <div class="condition">${course.condition}</div>
-    //             <div  class="img-item">
-    //                 <img src="${course.image.imgSub0}" alt="">
-    //             </div>
-    //             <div class="mt_10">
-    //                 <div class="name">${course.name}</div>
-    //                 <div class="color">${course.color}</div>
-    //                 <div class="price">${course.price}$</div>
-    //             </div>
-    //         </a>
-    //     </div>
-    //     `;
-    // })
-    // products.innerHTML = htmls.join('');
-    //             }
-    //         }
-    //     )
-    // }
-    // sortPrices();
+    function sortPrices() {
+        prices.addEventListener('change', ()=> {
+            const pricesValue = prices.value;
+                if (pricesValue == 'lowtohigh') {
+                    courses.sort((a, b) =>  
+                        a.price - b.price
+                    ) 
+                    const htmls = courses.map(course => { 
+                        return `
+                        <div class="product">
+                            <a href="#">
+                                <div class="condition">${course.condition}</div>
+                                <div  class="img-item">
+                                    <img src="${course.image.imgSub0}" alt="">
+                                </div>
+                                <div class="mt_10">
+                                    <div class="name">${course.name}</div>
+                                    <div class="color">${course.color}</div>
+                                    <div class="price">${course.price}$</div>
+                                </div>
+                            </a>
+                        </div>
+                        `;
+                    })
+                    products.innerHTML = htmls.join('');
+                }
+                if (pricesValue == 'hightolow') {
+                    courses.sort((a, b) =>  
+                        b.price - a.price
+                    ) 
+                    const htmls = courses.map(course => { 
+        return `
+        <div class="product">
+            <a href="#">
+                <div class="condition">${course.condition}</div>
+                <div  class="img-item">
+                    <img src="${course.image.imgSub0}" alt="">
+                </div>
+                <div class="mt_10">
+                    <div class="name">${course.name}</div>
+                    <div class="color">${course.color}</div>
+                    <div class="price">${course.price}$</div>
+                </div>
+            </a>
+        </div>
+        `;
+    })
+    products.innerHTML = htmls.join('');
+                }
+                if (pricesValue == "") {
+                    const htmls = courses.map(course => { 
+        return `
+        <div class="product">
+            <a href="#">
+                <div class="condition">${course.condition}</div>
+                <div  class="img-item">
+                    <img src="${course.image.imgSub0}" alt="">
+                </div>
+                <div class="mt_10">
+                    <div class="name">${course.name}</div>
+                    <div class="color">${course.color}</div>
+                    <div class="price">${course.price}$</div>
+                </div>
+            </a>
+        </div>
+        `;
+    })
+    products.innerHTML = htmls.join('');
+                }
+            }
+        )
+    }
+    sortPrices();
 
 }
 // lấy thông tin để lọc sản phẩm
-function getInfoFilter() {
-    var formInputs = {
-        clothing: '',
-        price: '',
-        size: '',
-        gender: '',
-        color: '',
-    }
-    if (inputForm) {
-        const selectedProducts = inputForm.querySelectorAll('[name]');
-        for (const selectedProduct of selectedProducts) {
-            formInputs = selectedProduct.value
-            selectedProduct.addEventListener('change', ()=> {
-            })
-        }
-    }
-}
-getInfoFilter();
-function showId(a) {
-    console.log(a);
+// function getInfoFilter() {
+//     var formInputs = {
+//         clothing: '',
+//         price: '',
+//         size: '',
+//         gender: '',
+//         color: '',
+//     }
+//     if (inputForm) {
+//         const selectedProducts = inputForm.querySelectorAll('[name]');
+//         for (const selectedProduct of selectedProducts) {
+//             formInputs = selectedProduct.value
+//             selectedProduct.addEventListener('change', ()=> {
+//             })
+//         }
+//     }
+// }
+// getInfoFilter();
+products.addEventListener('click', ()=> {
+    console.log(products.getAttribute('id'))
+})
+function showId(courses) {
+    courses.forEach(element => {
+       if (element.id) {
+           
+       }
+    });
   }
